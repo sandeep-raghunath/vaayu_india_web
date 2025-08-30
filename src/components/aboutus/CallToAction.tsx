6739ec17f9b85b2fb9803a24ed4df08ebf4b0440
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-08-30 22:33:56 +0530

    Update CallToAction.tsx

--- src/components/aboutus/CallToAction.tsx
+++ src/components/aboutus/CallToAction.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function CallToAction() {
   return (
-    <section className="mx-auto max-w-7xl rounded-2xl bg-gradient-to-b from-black to-blue-900 px-6 py-20 text-center">
-      <h2 className="mb-6 text-2xl font-semibold text-white md:text-3xl">
+    <section className="mx-auto mb-12 max-w-7xl rounded-2xl bg-gradient-to-b from-black to-blue-900 px-16 py-20 text-center">
+      <h2 className="mb-6 text-xl font-semibold text-white md:text-2xl">
         Let’s Meet And Talk About The Future <br /> Over A Cup Of Coffee!
       </h2>
       <button className="rounded-full bg-white px-6 py-3 font-medium text-black shadow-md transition hover:bg-gray-200">
