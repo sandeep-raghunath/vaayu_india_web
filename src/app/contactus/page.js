893112c6e9cbb1002c9fe51aa28b636158747180
Maneesh Kumar Pal <86200227+Austin73@users.noreply.github.com>
2025-09-06 17:44:49 +0530

    Update page.js

--- src/app/contactus/page.js
+++ src/app/contactus/page.js
@@ -1,10 +1,27 @@
 export const metadata = { title: "Contact Us" };
+import ContactMap from "@/components/contactus/ContactMap";
+import Partners from "@/components/contactus/Partners";
+import CallToAction from "@/components/aboutus/CallToAction";
+import ContactAddresses from "@/components/contactus/ContactAddresses";
+import ContactIntro from "@/components/contactus/ContactIntro";
+import BookCallWithExpert from "@/components/common/BookCallWithExpert";
 
 export default function ContactUs() {
+  // app/contact/page.tsx
   return (
-    <section className="container py-12">
-      <h1 className="mb-4 text-3xl font-bold">Contact Us</h1>
-      <p className="text-gray-700">Contact Us details here!!</p>
-    </section>
+    <main>
+      <div className="mx-auto mt-18 grid max-w-7xl items-start gap-12 px-6 py-16 md:grid-cols-2">
+        {/* Left Section */}
+        <ContactIntro />
+
+        <div className="rounded-xl border-[1px] border-gray-200 p-6">
+          <BookCallWithExpert />
+        </div>
+      </div>
+      <ContactAddresses />
+      <ContactMap />
+      <Partners />
+      <CallToAction />
+    </main>
   );
 }
